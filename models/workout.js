a6b08c5e3e65745c99557d05a3d9a82f51a308fc
Justin Bush <69733300+Justinbush24@users.noreply.github.com>
2021-02-24 19:55:56 -0600

    add models

---
+++ models/workout.js
@@ -0,0 +1,57 @@
+const { SCHED_NONE } = require("cluster");
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter an excercise type"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter an exercise name"
+        },
+        duration: {
+            type: Number,
+            required: "Enter an excercise duration in minutes"
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        }
+    }]
+}, {
+    toJSON: {
+        //include virtuals when requested
+        virtuals: true
+    }
+
+});
+
+// adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function() {
+    // reduce array of exercises down to just the sum of their durations
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;

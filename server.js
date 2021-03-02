a12d48c4262164fc8547fa5b5dd14dce57c96d9c
Justin Bush <69733300+Justinbush24@users.noreply.github.com>
2021-03-01 18:53:38 -0600

    minor debugging of routes

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 
 const db = require("./models");
 
-require(".routes/apiRoutes")(app);
-require(".routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 
 app.listen(PORT, () => {
